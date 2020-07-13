7d195203b7b74c5b1addebf798eb470f9e7e8714
nazar pavliuk <hesab.k18@gmail.com>
2020-07-14 02:36:08 +0300

    homework

---
+++ ClassWork/13_07_2020/MyBicycle/Bicycle.Web/Controllers/NazarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bicycle.Web.Controllers
+{
+    public class NazarController : Controller
+    {
+        // GET: Nazar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
