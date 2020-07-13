c61e91bc398ae23a9dd746d07b89ea2020196c60
nazar pavliuk <hesab.k18@gmail.com>
2020-07-14 02:40:06 +0300

    homework

---
+++ HomeWork/23_07_2020/23_07_2020/Controllers/MAINController.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23_07_2020.Controllers
+{
+    public class MAINController : Controller
+    {
+        // GET: MAIN
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HomeWork/23_07_2020/23_07_2020/Controllers/UserProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23_07_2020.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        // GET: UserProfile
+        public ActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
