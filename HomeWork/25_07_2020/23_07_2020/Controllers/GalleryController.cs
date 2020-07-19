0f6996915aa9e0570efd8be139d02dd3b804bb30
nazar pavliuk <hesab.k18@gmail.com>
2020-07-20 01:03:05 +0300

    homework

---
+++ HomeWork/25_07_2020/23_07_2020/Controllers/GalleryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23_07_2020.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public ActionResult Main()
+        {
+            Images im = new Images();
+            return View(im.Images_.ToArray());
+        }
+    }
+}
---
+++ HomeWork/25_07_2020/23_07_2020/Controllers/MAINController.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23_07_2020.Controllers
+{
+    public class MAINController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Images images = new Images();
+            //images.Images_.Add(new Image()
+            //{
+            //    Name="Name-1",
+            //    Path= "Content/Images/_1.jpeg"
+            //});
+            //images.Images_.Add(new Image()
+            //{
+            //    Name="Name-2",
+            //    Path= "Content/Images/_2.jpg"
+            //});
+            //images.Images_.Add(new Image()
+            //{
+            //    Name="Name-3",
+            //    Path= "Content/Images/_3.png"
+            //});
+            //images.Images_.Add(new Image()
+            //{
+            //    Name="Name-4",
+            //    Path= "Content/Images/_4.png"
+            //});
+            //images.Images_.Add(new Image()
+            //{
+            //    Name="Name-5",
+            //    Path= "Content/Images/_5.png"
+            //});
+            //images.Images_.Add(new Image()
+            //{
+            //    Name="Name-6",
+            //    Path= "Content/Images/_6.jpg"
+            //});
+            //images.SaveChanges();
+            return View();
+        }
+    }
+}
