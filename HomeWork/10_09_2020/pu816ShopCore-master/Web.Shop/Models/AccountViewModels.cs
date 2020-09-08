26dad907cce0acfafa4e9a62da4aba071af29ad8
Nazar <hesab.k18@gmail.com>
2020-09-09 01:47:19 +0300

    homework

---
+++ HomeWork/10_09_2020/pu816ShopCore-master/Web.Shop/Models/AccountViewModels.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Shop.Models
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Електронна адреса")]
+        [Required(ErrorMessage = "Вкажіть пошту")]
+        public string Email { get; set; }
+
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Вкажіть пароль")]
+        public string Password { get; set; }
+    }
+    public class RegistrationViewModel
+    {
+        [Display(Name = "User name")]
+        [Required(ErrorMessage = "Invalid user name")]
+        public string UserName { get; set; }
+        //
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Invalid email")]
+        public string Email { get; set; }
+        //
+        [Display(Name = "Phone number")]
+        [Required(ErrorMessage = "Invalid phone number")]
+        public string PhoneNumber { get; set; }
+        //
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Invalid password")]
+        public string Password { get; set; }
+    }
+}
